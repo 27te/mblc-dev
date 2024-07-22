7323133fc205d5347183e9fd8310e4f820abe482
Marco Antonio <marcoaranibar12@gmail.com>
2024-07-22 05:38:54 -0500

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  // site: 'url-hosting',
+  integrations: [tailwind(), sitemap()],
+  site: 'https://mblcdev.com/',
 });
