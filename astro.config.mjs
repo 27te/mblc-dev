d8bade9b9e3dab8b14805d6eca1dd158f4d98143
Marco Antonio <marcoaranibar12@gmail.com>
2024-07-30 00:30:54 -0500

    add pwa version base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,34 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import { VitePWA } from 'vite-plugin-pwa';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://mblcdev.com/',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mblcdev',
+        short_name: 'Mblcdev',
+        description:
+          'Desarrollador web freelancer con experiencia en el desarrollo de páginas web y sistemas de administración.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'mblcdev-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'mblcdev-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
