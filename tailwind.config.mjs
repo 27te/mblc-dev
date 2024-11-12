c6a671cfd4e80beba71b1f2fec4128df6a66136a
Antonio <marcoaranibar12@gmail.com>
2024-11-11 23:36:36 -0500

    feat(src): create a new web page
    new web page create and update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'satoshi-light': ['Satoshi-Light', 'sans-serif'],
+        'satoshi-lightitalic': ['Satoshi-LightItalic', 'sans-serif'],
+        'satoshi-regular': ['Satoshi-Regular', 'sans-serif'],
+        'satoshi-italic': ['Satoshi-Italic', 'sans-serif'],
+        'satoshi-medium': ['Satoshi-Medium', 'sans-serif'],
+        'satoshi-mediumitalic': ['Satoshi-MediumItalic', 'sans-serif'],
+        'satoshi-bold': ['Satoshi-Bold', 'sans-serif'],
+        'satoshi-bolditalic': ['Satoshi-BoldItalic', 'sans-serif'],
+        'satoshi-black': ['Satoshi-Black', 'sans-serif'],
+        'satoshi-blackitalic': ['Satoshi-BlackItalic', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
